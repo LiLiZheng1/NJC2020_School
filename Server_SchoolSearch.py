fdee8321af539ec45eeed98d02b96aa31fce0bb2
LiLiZheng1 <103553190+LiLiZheng1@users.noreply.github.com>
2022-04-13 12:40:51 +0800

    Add files via upload

---
+++ Server_SchoolSearch.py
@@ -0,0 +1,27 @@
+from flask import *
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("main.html")
+
+@app.route('/search',methods = ["POST"])
+def search():
+    data = request.form
+
+    school_name = data["School"]
+    department = data["Department"]
+
+    connection = sqlite3.connect("schools.db")
+    cursor = connection.execute("SELECT * FROM Schools,Staff WHERE Schools.SchoolName LIKE ? and Staff.Department = ?",("%" +school_name+"%",department)).fetchall()
+    connection.commit()
+
+    connection.close()
+
+    return render_template("results_teacher_found.html",cursor = cursor )
+
+
+if (__name__) == "__main__":
+    app.run(port = 5678, debug = True)
